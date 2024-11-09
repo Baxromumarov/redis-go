0a7ebcf25caa373c4ae8e850c10cd7a85ee581e1
baxromumarov <baxromumarov10@gmail.com>
2024-11-09 14:56:55 +0100

    init

--- cmd/main.go
+++ cmd/main.go
@@ -10,17 +10,22 @@ import (
 	"time"
 
 	dtb "github.com/baxromumarov/redis-go/database"
+	er "github.com/baxromumarov/redis-go/errors"
 )
 
-const (
-	QUIT = iota
-)
+type Connection struct {
+	conn              net.Conn
+	connType          string
+	activeConnections int
+	maxConnections    int
+	totalConnections  int
+}
 
 func main() {
 	fmt.Println()
 	db := dtb.Init()
-	
-	// in every two seconds, the cleaner will check for expired keys
+
+	// In every two seconds, the cleaner will check for expired keys
 	// it will work in background
 	db.StartExpirationCleaner(2 * time.Second)
 
@@ -31,9 +36,10 @@ func main() {
 	}
 	defer listener.Close()
 
-	fmt.Println("Server listening on port 6379...")
+	fmt.Println("Redis-go server listening on port 6060...")
 
-	ch := make(chan int)
+	ch := make(chan error)
+	defer close(ch)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -43,33 +49,38 @@ func main() {
 
 		go handleConnection(conn, db, &ch)
 		val := <-ch
-		fmt.Println(">>>>>>> ", val)
-		if val == QUIT {
-			return
+		switch val {
+
+		case er.ErrConnClosed:
+			fmt.Println("Connection closed")
+		default:
+			fmt.Print("Error reading message:", val)
 		}
+
 	}
 }
 
-func handleConnection(conn net.Conn, db *dtb.Database, ch *chan int) {
+func handleConnection(conn net.Conn, db *dtb.Database, ch *chan error) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
-
+	fmt.Println(conn.RemoteAddr().Network())
+	fmt.Println(conn.RemoteAddr().String())
 	for {
 		message, err := reader.ReadString('\n')
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				fmt.Println("here")
-				*ch <- QUIT
+
+				*ch <- er.ErrConnClosed
 				return
 			}
-			fmt.Println("Error reading message:", err)
-			return
+			*ch <- err
+
 		}
 
 		message = strings.TrimSpace(message)
 		response, err := dtb.HandleCommand(db, message)
 		if err != nil {
-			conn.Write([]byte("ERROR" + err.Error() + "\n"))
+			conn.Write([]byte("ERROR: " + err.Error() + "\n"))
 			continue
 		}
 
